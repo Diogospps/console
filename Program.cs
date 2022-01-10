c9ef3f6d5abce627f040f34004774e1c5228c9be
Diogospps <70813471+Diogospps@users.noreply.github.com>
2022-01-10 16:12:31 -0300

    comando de console .net

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int numeroDeVezez = 5;
+
+            for (int i= 0; i < numeroDeVezez; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
